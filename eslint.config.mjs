73eadc8c308a64934a13755d0308b42a960254e7
Suzy Kim <skme1103@naver.com>
2024-06-30 10:21:45 +0900

    eslint error solve

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,13 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+// import pluginJs from "@eslint/js";
+// import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+// import globals from "globals";
+// import tseslint from "typescript-eslint";
 
 
-export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  { files: ["**/*.jsx"], languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
-  pluginReactConfig,
-];
+// export default [
+//   {languageOptions: { globals: globals.browser }},
+//   pluginJs.configs.recommended,
+//   ...tseslint.configs.recommended,
+//   { files: ["**/*.jsx"], languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+//   pluginReactConfig,
+// ];
