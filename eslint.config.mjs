ca40159a6f225ac6fe9db8f5888a9bb6c299dfa9
Suzy Kim <skme1103@naver.com>
2024-06-30 10:31:55 +0900

    eslint settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,5 @@
+//해당 파일 때문에 'React' must be in scope when using JSXeslintreact/react-in-jsx-scope 에러 발생
+
 // import pluginJs from "@eslint/js";
 // import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 // import globals from "globals";
